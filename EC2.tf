f96981bee484f4e6a0c30c3650b97146fd0b5739
Emmylong1 <98855013+Emmylong1@users.noreply.github.com>
2023-04-04 10:54:22 +0000

    Update EC2.tf

--- EC2.tf
+++ EC2.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "web-server" {
-    ami = "xxxxxxx"
+    ami = "ami-007855ac798b5175e"
     instance_type = "t2.micro"
     subnet_id = aws_subnet.az1.id
    security_groups = [aws_security_group.allow_web.id]
